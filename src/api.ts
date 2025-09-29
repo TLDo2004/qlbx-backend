19c2a6b02200ae09042ab1cfeb1d7733d716db76
Liem Do <ncdongt@gmail.com>
2025-09-29 10:15:38 +0700

    config firebase

---
+++ src/api.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+import './firebase'; // Initialize Firebase
+// import routes from './routes';
+
+export const app = express();
+
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+app.use(express.raw({ type: 'application/vnd.custom-type' }));
+app.use(express.text({ type: 'text/html' }));
+
+// Healthcheck endpoint
+app.get('/', (req, res) => {
+  res.status(200).send({ status: 'ok' });
+});
+
+const api = express.Router();
+
+api.get('/hello', (req, res) => {
+  res.status(200).send({ message: 'hello world' });
+});
+
+// Mount all routes
+// api.use('/', routes);
+
+// Version the api
+app.use('/api/v1', api);
